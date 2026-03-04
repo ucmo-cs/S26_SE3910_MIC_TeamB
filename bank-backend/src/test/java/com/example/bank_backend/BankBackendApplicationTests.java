59c844de6288cea2929ced3d8aa6321e8dc37269
Danishk2445 <danishkhateeb03@gmail.com>
2026-03-04 07:46:04 -0600

    initialized backend

---
+++ bank-backend/src/test/java/com/example/bank_backend/BankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
